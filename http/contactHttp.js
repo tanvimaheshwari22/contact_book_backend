780d5ecd6899b88fe987895e73e3635e92ed644e
Tanvi <tanvi.maheshwari@argusoft.com>
2023-09-06 15:07:47 +0530

    create http layer for contacts method

---
+++ http/contactHttp.js
@@ -0,0 +1,22 @@
+import * as contactSvc from '../service/contactSvc.js'
+import express from 'express'
+
+const router = express.Router()
+
+router.get("/", async (req, res) => {
+    const users = await contactSvc.getUserContacts();
+    res.status(200).send({ data: users })
+})
+
+router.get("/all", async (req, res) => {
+    const users = await contactSvc.getUserContactById(req.query.user_id);
+    res.status(200).send({ data: users })
+})
+
+router.post("/", async (req, res) => {
+    const user = await contactSvc.addUserToContactList(req)
+
+    res.status(200).send({ isVerified: true, data: user })
+})
+
+export default router
