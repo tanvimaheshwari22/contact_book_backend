2399be838c2fb5ca2bfc827ae2cd1dd81fd9b291
Tanvi <tanvi.maheshwari@argusoft.com>
2023-09-20 15:29:57 +0530

    rearrange the code

--- repo/contactRepo.js
+++ repo/contactRepo.js
@@ -14,13 +14,12 @@ export const getUserContactByUserId = async (userId) => {
     return sequelize.query(`SELECT C.contact_user_id, C.user_id , U.mobile_number, U.first_name, U.last_name, U.status
                             FROM user_contacts AS C LEFT JOIN user_details AS U
                             ON U.user_id = C.contact_user_id
-                            WHERE C.user_id= ${userId}`);
+                            WHERE C.user_id = ${userId}`);
 }
 
 export const getSearchUsers = async (userId, searchValue) => {
     return sequelize.query(`SELECT C.contact_user_id, C.user_id , U.mobile_number, U.first_name, U.last_name, U.status
     FROM user_contacts AS C LEFT JOIN user_details AS U 
     ON U.user_id = C.contact_user_id
-    WHERE C.user_id= ${userId} AND (C.name LIKE '%${searchValue}%' OR U.mobile_number LIKE '%${searchValue}%')`);
+    WHERE C.user_id = ${userId} AND (C.name LIKE '%${searchValue}%' OR U.mobile_number LIKE '%${searchValue}%')`);
 };
-
