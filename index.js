f4b3c159b9ed70bff5bbc6f74ef29a40b533da8c
Tanvi <tanvi.maheshwari@argusoft.com>
2023-09-20 10:09:25 +0530

    format the code

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ import bodyParser from 'body-parser'
 import userRoutes from './http/userHttp.js'
 import contactRoutes from './http/contactHttp.js'
 import authenticateUser from './middleware/authentication.js';
+import cors from 'cors'
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
+
+app.use(cors({
+    origin: '*'
+}))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use("/users", userRoutes)
-app.use("/contacts", authenticateUser,contactRoutes)
+app.use("/contacts", authenticateUser, contactRoutes)
 
 app.listen(PORT, (error) => {
     if (!error)
