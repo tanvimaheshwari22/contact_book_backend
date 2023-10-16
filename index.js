04b794f2d777f8a4ab9a84837efdaa74fa10994b
tanvimaheshwari22 <tanvi.maheshwari@argusoft.com>
2023-10-16 14:43:16 +0530

    send update on client inactivity

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import userRoutes from './http/userHttp.js'
 import contactRoutes from './http/contactHttp.js'
 import loginRoutes from './http/loginHttp.js'
 import messageRoutes from './http/messageHttp.js'
+import groupRoutes from './http/groupHttp.js'
 import authenticateUser from './middleware/authentication.js';
 import cors from 'cors'
 import http from "http";
 import { WebSocketServer } from 'ws'
+import * as userRepo from './repo/userRepo.js'
+import * as contactRepo from "./repo/contactRepo.js"
 
 const app = express();
 const PORT = 5000;
@@ -16,9 +19,13 @@ const server = http.createServer(app);
 
 const wsServer = new WebSocketServer({ server });
 
-app.use(cors({
-    origin: '*'
-}))
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -30,8 +37,7 @@ app.use("/", loginRoutes)
 app.use("/users", authenticateUser, userRoutes)
 app.use("/contacts", authenticateUser, contactRoutes)
 app.use("/messages", authenticateUser, messageRoutes)
-
-
+app.use("/groups", authenticateUser, groupRoutes)
 
 server.listen(PORT, (error) => {
     if (!error)
@@ -46,9 +52,46 @@ export const CLIENTS = {};
 
 wsServer.on('connection', (ws, request) => {
     let userId
-    ws.on('message', (data) => {
+    ws.on('message', async (data) => {
         userId = JSON.parse(data.toString()).userId
+        userRepo.updateActivity(userId, "ONLINE")
         CLIENTS[userId] = ws;
+
+        for (const key in CLIENTS) {
+            const users = await contactRepo.getUserContactByUserId(key)
+            for (let i = 0; i < users[0].length; i++) {
+                // @ts-ignore
+                if (users[0][i].contact_user_id == userId) {
+                    CLIENTS[`${key}`].send(JSON.stringify({
+                        update: "activity updated",
+                        clientId: key
+                    }))
+                }
+            }
+        }
+    })
+
+    ws.on('close', async (data) => {
+        let curUser
+        for (const key in CLIENTS) {
+            if (CLIENTS[key]._closeCode === data) {
+                userRepo.updateActivity(key, "OFFLINE")
+                curUser = key
+                delete CLIENTS[`${key}`]
+            }
+        }
+        for (const key in CLIENTS) {
+            const users = await contactRepo.getUserContactByUserId(key)
+            for (let i = 0; i < users[0].length; i++) {
+                // @ts-ignore
+                if (users[0][i].contact_user_id == curUser) {
+                    CLIENTS[`${key}`].send(JSON.stringify({
+                        update: "activity updated",
+                        clientId: key
+                    }))
+                }
+            }
+        }
     })
 
     app.set("socket", ws)
